524fdbc729276dd7d6e53abd838ce50e4b07bfae
Adam Allen <allenac86@gmail.com>
2021-11-20 14:10:34 -0500

    remove unused data and imports

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const books = require("./routes/bookRoutes");
-const connect = require("./db/db");
-const cors = require("cors");
-const notFound = require("./middleware/notFound");
-const errorHandler = require("./middleware/errorHandler");
+const books = require('./routes/bookRoutes');
+const connect = require('./db/db');
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3000;
-require("dotenv").config();
+require('dotenv').config();
 
 // middleware
-app.use(cors()); // enables CORS for the front end eventually
+// app.use(cors()); // will enable CORS for the front end eventually
 app.use(express.json());
 
 // routes
-/**
- * redirect to resume route for now
- * come back to root path once front end skeleton is setup to
- * determine what data will be used on the landing page
- */
-app.get("/", (req, res) => {
-  res.json({ msg: "api v1 test root path" });
+app.get('/', (req, res) => {
+	res.redirect('/api/v1/books');
 });
 
-app.use("/api/v1/books", books);
+app.use('/api/v1/books', books);
 
 // 404 middleware
 app.use(notFound);
 app.use(errorHandler);
 
 const start = async () => {
-  try {
-    await connect(process.env.MONGO_URI);
-    app.listen(PORT, console.log(`Server is listening on port: ${PORT} ...`));
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await connect(process.env.MONGO_URI);
+		app.listen(PORT, console.log(`Server is listening on port: ${PORT} ...`));
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 start();
