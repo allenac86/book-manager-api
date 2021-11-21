73e465d5f4502105481d3a4129303aa2905c740d
Adam Allen <allenac86@gmail.com>
2021-11-20 19:39:54 -0500

    added collection length to JSON output in getAllBooks controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,60 +1,61 @@
-const Book = require("../models/Book");
-const asyncHandler = require("../middleware/asyncHandler");
-const { createCustomError } = require("../errors/CustomError");
+const Book = require('../models/Book');
+const asyncHandler = require('../middleware/asyncHandler');
+const { createCustomError } = require('../errors/CustomError');
 
 const getAllBooks = asyncHandler(async (req, res) => {
-  const books = await Book.find({});
-  res.status(200).json({ books });
+	const books = await Book.find({});
+	res.status(200).json({ numBooks: books.length, books });
 });
 
 const createBook = asyncHandler(async (req, res) => {
-  const book = await Book.create(req.body);
-  res.status(201).json({ book });
+	const book = await Book.create(req.body);
+	res.status(201).json({ book });
 });
 
 const getBook = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const book = await Book.findOne({ _id: id });
+	const { id } = req.params;
+	const book = await Book.findOne({ _id: id });
 
-  if (!book) {
-    return next(createCustomError(`No book found with id: ${id}`, 404));
-  }
+	if (!book) {
+		return next(createCustomError(`No book found with id: ${id}`, 404));
+	}
 
-  res.status(200).json({ book });
+	res.status(200).json({ book });
 });
 
-// PATCH - will only update properties with values provided
+// PATCH - will only update the provided properties
+// will not replace the entire object
 const updateBook = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const book = await Book.findOneAndUpdate({ _id: id }, req.body, {
-    new: true,
-    runValidators: true,
-    // overwrite: true // uncomment if you prefer PUT instead of PATCH in routes
-  });
+	const book = await Book.findOneAndUpdate({ _id: id }, req.body, {
+		new: true,
+		runValidators: true,
+		// overwrite: true // uncomment if you prefer PUT instead of PATCH in routes
+	});
 
-  if (!book) {
-    return next(createCustomError(`No book found with id: ${id}`, 404));
-  }
+	if (!book) {
+		return next(createCustomError(`No book found with id: ${id}`, 404));
+	}
 
-  res.status(200).json({ book });
+	res.status(200).json({ book });
 });
 
 const deleteBook = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const book = await Book.findOneAndDelete({ _id: id });
+	const { id } = req.params;
+	const book = await Book.findOneAndDelete({ _id: id });
 
-  if (!book) {
-    return next(createCustomError(`No book found with id: ${id}`, 404));
-  }
+	if (!book) {
+		return next(createCustomError(`No book found with id: ${id}`, 404));
+	}
 
-  res.status(200).json({ message: "book deleted", book });
+	res.status(200).json({ message: 'book deleted', book });
 });
 
 module.exports = {
-  getAllBooks,
-  createBook,
-  getBook,
-  updateBook,
-  deleteBook,
+	getAllBooks,
+	createBook,
+	getBook,
+	updateBook,
+	deleteBook,
 };
